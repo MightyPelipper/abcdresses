72f7a3c48c122133dfd5c62486713b0f46a22268
Samir <39209311+MightyPelipper@users.noreply.github.com>
2020-03-03 16:34:50 -0600

    Prefrences, index and Bootstrap Updates
    Implemented Bootstrap files into the project natively. Created Carousel view for index page. Updated prefrences to work with index page.

--- index.php
+++ index.php
@@ -63,8 +63,45 @@ table.center {
 </style>
 </head>
 
+        <!--This is for bootstrap for carousel-->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</head>
+    
+
 <body>
 <h2 style = "color: #01B0F1;">Welcome to ABC Dresses </h3>
+
+
+<!--style for indicator on carosel-->
+<style>
+.carousel-control-prev-icon,
+.carousel-control-next-icon {
+  height: 100px;
+  width: 100px;
+  outline: black;
+  background-size: 100%, 100%;
+  border-radius: 50%;
+  border: 1px solid black;
+  background-color: black;
+}
+
+.carousel-control-next-icon:after
+{
+  content: '';
+  font-size: 55px;
+  color: red;
+}
+
+.carousel-control-prev-icon:after {
+  content: '';
+  font-size: 55px;
+  color: red;
+}
+
+</style>
 
 <!--Displaying images in grid view-->
 
@@ -83,6 +120,7 @@ table.center {
 
     $sql1 = "SELECT `value` FROM `preferences` WHERE `name`= 'NO_OF_DRESSES_PER_ROW'";
     $sql4 = "SELECT `value` FROM `preferences` WHERE `name`= 'NO_OF_DRESSES_TO_SHOW'";
+    $sql5 = "SELECT `comments` FROM `preferences` WHERE `name`= 'DEFAULT_VIEW_FOR_HOME_PAGE'";
     //this is to get the numbers of questions from preferences so i can limit the number of questions printed
     //$sqlpref = "SELECT `value` FROM `preferences` WHERE `name`= 'NO_OF_QUESTIONS_TO_SHOW'";
     //$resultspref = mysqli_query($db,$sqlpref);
@@ -100,6 +138,8 @@ table.center {
     $results2 = mysqli_query($db,$sql2);
     $results3 = mysqli_query($db,$sql3);
     $results4 = mysqli_query($db,$sql4);
+    $results5 = mysqli_query($db,$sql5);
+
 
     if(mysqli_num_rows($results1)>0){
         while($row = mysqli_fetch_assoc($results1)){
@@ -126,6 +166,12 @@ table.center {
         }
     }
 
+    if(mysqli_num_rows($results5)>0){
+        while($row = mysqli_fetch_assoc($results5)){
+            $view[] = $row;
+        }
+    }
+
 
 
     $columns = $column[0]['value'];
@@ -133,7 +179,13 @@ table.center {
     $numDresses = $numDress[0]['value'];
 
     $count= count($dresses);
+
+    $defaultView = $view[0]['comments'];
+
+
     
+    if( $defaultView == 'Grid'){  //if view is set to GRID
+
     echo "<table id = 'table_2'>
     <!--Links to quizzes can be put inside the href = -->";
     echo "<tr>";
@@ -159,6 +211,52 @@ table.center {
     echo "</tr>";
     }   
     echo"</table>";
+    }
+
+   if( $defaultView == 'Carousal'){  // if the view is set to carousal
+
+    //echo "it works";
+
+
+    echo "<div id='carouselExampleControls' class='carousel slide' data-ride='carousel'>
+    <div class='carousel-inner'>
+      <div class='carousel-item active'>
+        <img src='dress_images/crop_top_girl.jpg' class='d-block w-100' alt='...'>
+      </div>";
+
+    
+
+
+    for ($a=0; $a<$count; $a++){
+
+        //loop through all the images
+        $pic = $pics[$a]['image_url'];
+        echo "<div class='carousel-item'>
+        <img src='dress_images/$pic' class='d-block w-100' alt='$pic'>
+      </div>";
+    
+    }
+    
+    echo "</div>
+    <a class='carousel-control-prev' href='#carouselExampleControls' role='button' data-slide='prev'>
+      <span class='carousel-control-prev-icon' aria-hidden='true'></span>
+      <span class='sr-only'>Previous</span>
+    </a>
+    <a class='carousel-control-next' href='#carouselExampleControls' role='button' data-slide='next'>
+      <span class='carousel-control-next-icon' aria-hidden='true'></span>
+      <span class='sr-only'>Next</span>
+    </a>
+  </div>";
+
+   } 
+
+   else{
+
+        if($defaultView == 'list'){
+       echo "broken no list exsists yet in the code";}
+       // as of now list view does not exist
+   }
+
     ?>
 
 
--- preferences.php
+++ preferences.php
@@ -1,17 +1,23 @@
 <?php 
 require 'bin/functions.php';
 require 'db_configuration.php';
-$page_title = 'Quiz Master > Preferences';
+$page_title = 'ABC Dresses > Preferences';
+include('nav.php');
 include('header.php'); 
     $page="questions_list.php";
     verifyLogin($page);
 
-$sql1 = "SELECT `value` FROM `preferences` WHERE `name`= 'NO_OF_TOPICS_PER_ROW'";
-$sql2 = "SELECT `value` FROM `preferences` WHERE `name`= 'NO_OF_QUESTIONS_TO_SHOW'";
+$sql1 = "SELECT `value` FROM `preferences` WHERE `name`= 'NO_OF_DRESSES_PER_ROW'";
+$sql2 = "SELECT `value` FROM `preferences` WHERE `name`= 'NO_OF_DRESSES_TO_SHOW'";
+$sql3 = "SELECT `comments` FROM `preferences` WHERE `name`= 'NAME_OF_FAVORITE_DRESS'";
+$sql4 = "SELECT `comments` FROM `preferences` WHERE `name`= 'DEFAULT_VIEW_FOR_HOME_PAGE'";
 
 $results = mysqli_query($db,$sql1);
 $results2 = mysqli_query($db,$sql2);
+$results3 = mysqli_query($db,$sql3);
+$results4 = mysqli_query($db,$sql4);
 
+//gets number of rows
 if(mysqli_num_rows($results)>0){
     while($row = mysqli_fetch_assoc($results)){
         $column[] = $row;
@@ -19,17 +25,35 @@ if(mysqli_num_rows($results)>0){
 }
 $rows = $column[0]['value'];
 
+//gets number of dresses
 if(mysqli_num_rows($results2)>0){
     while($row = mysqli_fetch_assoc($results2)){
-        $question[] = $row;
+        $dresses[] = $row;
     }
 }
-$questions = $question[0]['value'];
+$dresses = $dresses[0]['value'];
+
+//gets favorite dress
+if(mysqli_num_rows($results3)>0){
+    while($row = mysqli_fetch_assoc($results3)){
+        $favorite[] = $row;
+    }
+}
+$favorite = $favorite[0]['comments'];
+
+//gets default view for home page
+if(mysqli_num_rows($results4)>0){
+    while($row = mysqli_fetch_assoc($results4)){
+        $defaultView[] = $row;
+    }
+}
+$defaultView = $defaultView[0]['comments'];
+
 ?>
 <style>#title {text-align: center;color: darkgoldenrod;}</style>
 <html>
     <head>
-        <title>QuizMaster Quiz</title>
+        <title>ABC Dresses</title>
         <style>
         input {
             text-align: center;
@@ -43,7 +67,7 @@ $questions = $question[0]['value'];
     <div class="container">
         <!--Check the CeremonyCreated and if Failed, display the error message-->
         
-        <form action="modifyThePreferences.php" method="POST">
+        <form action="modify_the_preferences.php" method="POST">
         <table style="width:500px">
         <tr>
             <th style="width:200px"></th>
@@ -57,9 +81,37 @@ $questions = $question[0]['value'];
         </tr>
         <tr>
             <td style="width200px">Number of questions to show:</td>
-            <td><input disabled type="int" maxlength="2" size="10" value="<?php echo $questions; ?>" title="Current value"></td> 
-            <td><input required type="int" name="new_questions" maxlength="2" size="10" title="Enter a number"></td>
+            <td><input disabled type="int" maxlength="2" size="10" value="<?php echo $dresses; ?>" title="Current value"></td> 
+            <td><input required type="int" name="new_dresses" maxlength="2" size="10" title="Enter a number"></td>
         </tr>
+
+        <tr>
+            <td style="width200px">Name of favorite dress:</td>
+            <td><input disabled type="text" maxlength="20" size="10" value="<?php echo $favorite; ?>" title="Current value"></td> 
+            <td><input required type="text" name="new_favorite" maxlength="20" size="10" title="Enter a dress name"></td>
+            
+        </tr>
+        
+        
+
+
+
+        <tr>
+            <td style="width200px">Default view for home page:</td>
+            <td><input disabled type="text" maxlength="20" size="10" value="<?php echo $defaultView; ?>" title="Current value"></td> 
+            
+
+            <td><select type="text" name="new_defaultView" title="Enter view type">
+                <option value="Grid">Grid</option>
+                <option value="List">List</option>
+                <option value="Carousal">Carousal</option>
+            
+            </select></td>
+        </tr>
+
+
+
+
         </table><br>
         <button type="submit" name="submit" class="btn btn-primary btn-md align-items-center">Modify Preferences</button>
         </form>
